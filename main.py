6c6c2b290204904ecec81d0381530281a0d72678
YC_246 <73423285+Unsunglegend@users.noreply.github.com>
2021-08-18 12:11:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+#This prog. file shows, how to add comment
+"""USE OF  &  3 times quotes,
+    can make a line into comment"""
+#PRINT STATEMENT WITH END FUNCTION:
+print("You are using Pycharm now")
+print("don't forget to save the program")
+
+print("You are using Pycharm now", end=" AND ")
+print("don't forget to save the program")
+
+print("You are using Pycharm now","don't forget to save the program")
+
+
+
+"""15 June 2021 (School)"""
+print("Abhigyan Srivastav")
+print("Abhigyan \nSrivastav")
+print('Abhigyan','Srivastav')
+print('Abhigyan','Srivastav',sep="_")
+
+"""16 June 2021"""
+a=15
+b=10
+c=a+b
+print("sum=",c)
+
+a,b=0,-1
+c=a+b
+print(c)
+
+m,n=-123*2,4
+o=m+n
+print(o)
+
+c,d=4,3
+e=c**d
+print(e)
+
+"""12 Aug 2021"""
+a=10
+b=20
+c=50
+c=a*b
+print("Product of numbers")
+print(c)
+a=10
+b=20
+c=50
+c=a/b
+print("Division of numbers")
+print(c)
+#Now we want only one print function with both quote and output
+a=10
+b=20
+d=50
+c=a*b
+print("Product of numbers",d,c)
+#Power:
+a=10**3
+print(a)
+#Floor Divison
+a=10
+b=20
+c=a//b
+print("Floor Division of numbers")
+print(c)
+c=100//6
+print(c)
+
+"""
+Any thing which is written in double quoted comma is called string
+    """
+f=input("u can type anyhting here")
+print(f)
+#only print integers:
+f=int(input("Type any integer"))
+print(f)
+#Mini calculator for sum of integers
+f=int(input())
+a=int(input())
+c=a+f
+print(c)
+
+
+
+
